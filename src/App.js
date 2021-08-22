eafa5363667ced9a638847c67eb176f0682d4880
Arya Anish <anishkumarsarmi@gmail.com>
2021-08-22 14:50:09 +0530

    Added Rick Roll

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './home/Home';
 import Contact from './contact/Contact';
 import Projects from './project/Projects';
 import Navbar from './Navbar';
+import RickRoll from './RickRoll';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/projects" exact component={Projects} />
+        <Route path="/await" exact component={RickRoll} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/RickRoll.js
@@ -0,0 +1,5 @@
+const RickRoll = () => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+};
+
+export default RickRoll;
